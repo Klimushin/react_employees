7a35e3ae20cc053a5e3c0668a5cedf547ffee925
Seroga <seroga2801@gmail.com>
2022-06-29 22:17:02 +0300

    less130 add filter functions

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -17,7 +17,9 @@ class App extends Component {
                 {name: 'Piter Parker', salary: 800, increase: false, favorite: false, id: 1},
                 {name: 'Anna Pitson', salary: 1500, increase: false, favorite: false, id: 2},
                 {name: 'Will Smith', salary: 2000, increase: false, favorite: false, id: 3},
-            ]
+            ],
+            term: '',
+            filter: 'all'
         }
     }
 
@@ -69,22 +71,55 @@ class App extends Component {
         }))
     }
     
-        
-    
+    searchEmp = (items, term) => {
+        if (term.length === 0) {
+            return items
+        }
+
+        return items.filter(item => {
+            return item.name.indexOf(term) > -1
+        })
+    }   
+
+    onUpdateSearch = (term) => {
+        this.setState({term})
+    }
+
+    filterBlock = (items, filter) => {
+        switch (filter) {
+            case 'rise':
+                return items.filter(item => item.favorite)
+            case 'moreThan1000':
+                return items.filter(item => item.salary > 1000)
+            default:
+                return items
+        }
+    }
+
+    onFilterSelect = (filter) => {
+        this.setState({filter})
+    }
         
     render() {
+        const {data, term, filter} = this.state
         const employees = this.state.data.length
         const increased = this.state.data.filter(item => item.increase).length
+        const visibleData = this.filterBlock(this.searchEmp(data, term), filter)
         return (            
             <div className="app">
                 <AppInfo employees={employees} increased={increased}/>
     
                 <div className='search-panel'>
-                    <SearchPanel/>
-                    <AppFilter/>
+                    
+                    <SearchPanel 
+                    onUpdateSearch={this.onUpdateSearch}
+                    />
+
+                    <AppFilter filter={filter} onFilterSelect={this.onFilterSelect}/>
+
                 </div>
                 <EmployeesList
-                    data={this.state.data}
+                    data={visibleData}
                     onDelete={this.deleteItem}
                     onToggleIncrease={this.onToggleIncrease}
                     onToggleRise={this.onToggleRise}/>
